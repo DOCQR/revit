979a3522b005322a7fb0a083cd94b5debc321004
CharlieP <charlie.portelli@gmail.com>
2015-09-26 15:08:28 -0400

    Extracted View Port Info

--- DOCQR.Revit/Command.cs
+++ DOCQR.Revit/Command.cs
@@ -14,7 +14,7 @@ namespace DOCQR.Revit
     [Transaction(TransactionMode.Manual)]
     public class Upload : IExternalCommand
     {
-        
+
 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -41,6 +41,55 @@ namespace DOCQR.Revit
 
         }
 
-    }
 
-}
+        /// <summary>
+        /// Get the project sheets and the views on the sheets
+        /// </summary>
+        private void GetViews(Document doc)
+        {
+            FilteredElementCollector col = new FilteredElementCollector(doc);
+            List<Element> Elements = new List<Element>();
+            Elements.AddRange(col.OfClass(typeof(ViewSheet)).ToElements());
+
+
+            List<ElementId> SheetIDs = new List<ElementId>();                   // the list of sheet id's
+            List<List<ElementId>> ViewPortIDs = new List<List<ElementId>>();    // each sheet might have more the one view on it
+            List<List<Guid>> ViewPortGuids = new List<List<Guid>>();
+            List<List<XYZ>> ViewPortLocation = new List<List<XYZ>>();
+
+
+            foreach (Element ele in Elements)
+            {
+                ViewSheet TempSheet = (ViewSheet)ele;           // convert element to view sheet
+                SheetIDs.Add(TempSheet.Id);                     // extract sheet ID
+
+                List<Guid> TempViewPortGuids = new List<Guid>();  // temporary list for view guids
+                List<ElementId> TempViewPortIDs = new List<ElementId>();
+                List<XYZ> TempViewPortLocation = new List<XYZ>();
+
+                // for each sheet extract each view port
+                foreach (View v in TempSheet.Views)
+                {
+                    Guid guid = new Guid(v.UniqueId);
+                    TempViewPortGuids.Add(guid);                    // get the view port guid
+
+                    TempViewPortIDs.Add(v.Id);                      // get the view port id
+
+                    LocationPoint lp = (LocationPoint)v.Location;   // get the view port start point
+                    TempViewPortLocation.Add(lp.Point);
+                }
+
+                ViewPortGuids.Add(TempViewPortGuids);               // save all the guids
+                ViewPortIDs.Add(TempViewPortIDs);                   // save all the ids
+                ViewPortLocation.Add(TempViewPortLocation);         // save all the location points
+            }
+        }       // close function
+
+
+        // view port id - element ID, guid
+        // view port location - xyz
+        // sheet id
+
+
+    }       // close class
+}           // close namespace
