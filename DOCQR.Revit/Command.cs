f17e1728a7e89c1a75a900b1bd2fb6683c39f212
mattmas <mmason@rand.com>
2015-09-26 14:20:23 -0400

    intial inclusion of Spectacle exporter libraries and Assembly resolution

--- DOCQR.Revit/Command.cs
+++ DOCQR.Revit/Command.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using Autodesk.Revit;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
@@ -14,13 +15,17 @@ namespace DOCQR.Revit
     [Transaction(TransactionMode.Manual)]
     public class Upload : IExternalCommand
     {
-        
+        private UIApplication _uiApp;
 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIApplication uiApp = commandData.Application;
+            _uiApp = commandData.Application;
             Document doc = uiApp.ActiveUIDocument.Document;
             UIDocument uidoc = uiApp.ActiveUIDocument;
+            
+            // take care of AppDomain load issues
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
 
             try
             {
@@ -39,6 +44,31 @@ namespace DOCQR.Revit
 
             return Result.Succeeded;
 
+        }
+
+        System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            // if the request is coming from us
+            if ((args.RequestingAssembly != null) && (args.RequestingAssembly == this.GetType().Assembly))
+            {
+                if ((args.Name != null) && (args.Name.Contains(","))  // ignore resources and such
+                {
+                    string asmName = args.Name.Split(',')[0];
+
+                    string targetFilename = Path.Combine( System.Reflection.Assembly.GetExecutingAssembly().Location, asmName + ".dll" );
+
+                    _uiApp.Application.WriteJournalComment("Assembly Resolve issue. Looking for: "  +args.Name, false );
+                    _uiApp.Application.WriteJournalComment("Looking for " + targetFilename, false);
+                    
+                    if (File.Exists( targetFilename ))
+                    {
+                        _uiApp.Application.WriteJournalComment("Found, and loading...", false);
+                        return System.Reflection.Assembly.LoadFrom( targetFilename );
+                    }
+                }
+            }
+
+            return null;
         }
 
     }
