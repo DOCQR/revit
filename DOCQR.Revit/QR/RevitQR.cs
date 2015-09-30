97c736c98fcd0985324c02b19d9ff97ba8c5a964
moethu <maximilian.o.e.thumfart@gmail.com>
2015-09-30 11:20:51 +0100

    QR Optimization

--- DOCQR.Revit/QR/RevitQR.cs
+++ DOCQR.Revit/QR/RevitQR.cs
@@ -13,24 +13,17 @@ namespace DOCQR.Revit
 {
     public class RevitQR
     {
-
-        private static FilledRegionType solidFill;
-
-
         private string URL;
 
         private string data;
 
         private string groupname;
 
-        public RevitQR(Document doc, SheetInfo info, bool asImage, string url)
+        public RevitQR(Document doc, SheetInfo info, string url)
         {
-            if (asImage)
-            {
-                FilteredElementCollector fec = new FilteredElementCollector(doc).OwnedByView(info.sheetId).OfCategory(BuiltInCategory.OST_RasterImages);
-                foreach (Element element in fec.ToElements()) { if (element.Name.EndsWith("QRTAG")) doc.Delete(element.Id); }
-            }
-
+            // Erase all QRTAG Images from the Sheet
+            FilteredElementCollector fec = new FilteredElementCollector(doc).OwnedByView(info.sheetId).OfCategory(BuiltInCategory.OST_RasterImages);
+            foreach (Element element in fec.ToElements()) { if (element.Name.EndsWith("QRTAG")) doc.Delete(element.Id); }
 
             this.URL = url;
 
@@ -39,66 +32,49 @@ namespace DOCQR.Revit
                 // Assemble URL
                 data = String.Format("{0}/viewer/{1}", new object[] { URL, vport.docQRid });
 
-                // Assemble Group 
-                groupname = String.Format("DOCQR-{0}", vport.id.IntegerValue.ToString());
-
-
-                if (!asImage)
-                    HatchTag(doc, info, vport);
-                else
-                    ImageTag(doc, info, vport);
+                // Create an image Tag
+                ImageTag(doc, info, vport);
             }
         }
 
         public void ImageTag(Document doc, SheetInfo info, ViewPortInfo vport)
         {
-
             // Create a new QRCoder Tag
             QRCoder.QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+
+            // Generate a new QR Tag
             QRCoder.QRCodeGenerator.QRCode qrCode = qrGenerator.CreateQrCode(data, QRCoder.QRCodeGenerator.ECCLevel.M);
+
+            // Assemble a temp file name
             string tempfile = System.IO.Path.GetTempFileName() + ".QRTAG";
+
+            // Get the Tags graphics element
             System.Drawing.Bitmap bmp = qrCode.GetGraphic(5);
+
+            // Save it temporarily
             bmp.Save(tempfile);
+
+            // Get the related view sheet
             View sheet = (View)doc.GetElement(info.sheetId);
+
+            // Create a image element to return from import
             Element imageElement = null;
 
-            //XYZ location = new XYZ(vport.location.X + bmp.Width / 250, vport.location.Y - bmp.Height / 250, 0);
+            // Get the Viewports label outline
             Outline labelOutline = vport.vport.GetLabelOutline();
+
+            // Get the lower left corner
             XYZ labelCorner = new XYZ(info.sheet.Origin.X + (labelOutline.MinimumPoint.X), info.sheet.Origin.Y + labelOutline.MinimumPoint.Y, 0);
+
+            // Add some distance value
             XYZ location = labelCorner.Add(new XYZ(0, -0.5/12.0, 0));
 
+            // Import the image
             doc.Import(tempfile, new ImageImportOptions() { RefPoint = location, Placement = BoxPlacement.TopLeft }, sheet, out imageElement);
             
-            string n = imageElement.GetType().ToString();
-
+            // Delete temp file
+            if (System.IO.File.Exists(tempfile)) System.IO.File.Delete(tempfile);
         }
-
-
-        public void HatchTag(Document doc, SheetInfo info, ViewPortInfo vport)
-        {
-            // Get Filled Region
-            FilteredElementCollector col = new FilteredElementCollector(doc);
-            col.OfClass(typeof(Autodesk.Revit.DB.FilledRegionType));
-
-            if (solidFill == null)
-            {
-                FillPatternElement elem = FillPatternElement.GetFillPatternElementByName(doc, FillPatternTarget.Drafting, "Solid fill");
-                solidFill = col.FirstElement() as FilledRegionType;
-                solidFill.Background = FilledRegionBackground.Opaque;
-                solidFill.FillPatternId = elem.Id;
-            }
-
-            // Create a new QRCoder Tag
-            QRCoder.QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
-            QRCoder.QRCodeGenerator.QRCode qrCode = qrGenerator.CreateQrCode(data, QRCoder.QRCodeGenerator.ECCLevel.M);
-
-            qrCode.GetGraphic(1).Save(System.IO.Path.GetTempFileName());
-
-        }
-
-
-
-
 
 
     }
